b3c370c32de85d2de09b0e7ed5f362c65f5ad798
Elias Hall <elias.hall@gmail.com>
2021-12-26 16:11:43 -0600

    Initial Commit

---
+++ SandwichPlateUITests/SandwichPlateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SandwichPlateUITestsLaunchTests.swift
+//  SandwichPlateUITests
+//
+//  Created by Elias Hall on 12/26/21.
+//
+
+import XCTest
+
+class SandwichPlateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
